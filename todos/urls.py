565d524035a1d5977564ad6c3162406c75d265b3
hyoenah <kkongc7@gmail.com>
2019-10-02 16:50:05 +0900

    edit|update 완성

--- todos/urls.py
+++ todos/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+
     path('new/', views.new),
     path('create/', views.create),
 
+    path('<int:id>/delete/', views.delete),
 
-    path('<int:id>/delete', views.delete),
+    path('<int:id>/edit/', views.edit),
+    path('<int:id>/update/', views.update),
 ]
